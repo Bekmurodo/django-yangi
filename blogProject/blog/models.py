5dcd20c94daa8ef0384b8b53536c3da0b1b53bcb
Bekmurodo <zsuhrob240@gmail.com>
2024-07-05 19:59:53 +0300

    daily cods
    my new blog project

--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render
+
+
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
@@ -8,4 +12,15 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+def Blogdetailview(request, id):
+    try:
+        blog = Blog.objects.get(id=id)
+        context = {
+            'blog': blog,
+        }
+    except Blog.DoesNotExist:
+        raise Http404('no blog found')
+
+    return render(request, 'blog_detail.html', context=context)
 
