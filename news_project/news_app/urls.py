2bac2f49d3f603356b38e3f5baa5bd34cf704e3f
Bekmurodo <zsuhrob240@gmail.com>
2024-08-13 20:49:09 +0300

    Update urls.py

--- news_project/news_app/urls.py
+++ news_project/news_app/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('news/', news_list, name="al_news_list"),
     path('news/<int:id>', news_detail, name="news_detail_page"),
     path('contact/', contactPageView, name='contact_page'),
-    path('404', viewPage404, name='404')
+    path('404', viewPage404, name='404_page')
 
 ]
