4c2a9e0e3c6567ffa26f6dfe6eb85b7397172c22
Bekmurodo <zsuhrob240@gmail.com>
2024-09-26 23:13:48 +0300

    Update urls.py

--- news_project/news_app/urls.py
+++ news_project/news_app/urls.py
@@ -3,7 +3,12 @@ from .views import (
     news_list,
     news_detail,
     homePageView,
-    ContactPageView
+    ContactPageView,
+    HomePageVIew,
+    LocalNewsView,
+    ForeignNewsView,
+    TechnologyNewsView,
+    SportNewsView
 
 )
 
@@ -11,10 +16,13 @@ from .views import (
 
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("", HomePageVIew.as_view(), name="home"),
     path('news/', news_list, name="al_news_list"),
-    path('news/<int:id>', news_detail, name="news_detail_page"),
+    path('news/<slug:news>/', news_detail, name="news_detail_page"),
     path('contact/', ContactPageView.as_view(), name='contact_page'),
-
+    path('local/', LocalNewsView.as_view(), name="local_news_page"),
+    path('foreign/', ForeignNewsView.as_view(), name="foreign_news_page"),
+    path('technology/', TechnologyNewsView.as_view(), name="technology_news_page"),
+    path('sport/', SportNewsView.as_view(), name="sport_news_page")
 
 ]
