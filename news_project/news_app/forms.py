c404db1ae0732ffb365daa9ae5be46da34ed8975
Bekmurodo <zsuhrob240@gmail.com>
2024-12-01 21:46:12 +0300

    Update forms.py

--- news_project/news_app/forms.py
+++ news_project/news_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Contact
+from .models import Contact, Comment
 
 class ContactForm(forms.ModelForm):
 
@@ -11,3 +11,9 @@ class SubscriptionForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField()
     email = forms.EmailField()
+
+class CommentForm(forms.Form):
+
+    class Meta:
+        model = Comment
+        fields = ['user', 'body']
